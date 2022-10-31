0639aa58fbc17910db817231618aac01bc50040b
anicholls41 <anicholls41@icloud.com>
2022-10-31 11:54:19 +0000

    Adam Solution

--- src/main/java/GuessTheNumberGame.java
+++ src/main/java/GuessTheNumberGame.java
@@ -1,5 +1,9 @@
+import game.Game;
+import game.GuessTheNumber;
+
 public class GuessTheNumberGame {
     public static void main(String[] args) {
-
+        Game game = new GuessTheNumber(1, 10);
+        game.play();
     }
 }
--- src/main/java/utils/IO.java
+++ src/main/java/utils/IO.java
@@ -1,5 +1,18 @@
 package utils;
 
-public abstract class IO {
+import java.util.Scanner;
 
+public abstract class IO {
+    private final Scanner in;
+
+    protected IO(){
+        in = new Scanner(System.in);
+    }
+
+    protected String getUserInput(String msg){
+        System.out.printf("%s >> ", msg);
+        String input = in.nextLine();
+        System.out.println();
+        return input;
+    }
 }
